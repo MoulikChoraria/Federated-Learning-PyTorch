d1692f06391b5936fb1b68b55ce91ac262b43d3b
MoulikChoraria <moulik.choraria@gmail.com>
2022-08-19 15:12:38 -0500

    "fairness code v0"

--- src/models.py
+++ src/models.py
@@ -6,6 +6,21 @@ from torch import nn
 import torch.nn.functional as F
 
 
+class Linear(nn.Module):
+    def __init__(self, dim_in, dim_out):
+        super(MLP, self).__init__()
+        self.layer_input = nn.Linear(dim_in, dim_out)
+        #self.relu = nn.ReLU()
+        #self.dropout = nn.Dropout()
+        #self.layer_hidden = nn.Linear(dim_hidden, dim_out)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = x.view(-1, x.shape[1]*x.shape[-2]*x.shape[-1])
+        x = self.layer_input(x)
+        return self.softmax(x)
+
+
 class MLP(nn.Module):
     def __init__(self, dim_in, dim_hidden, dim_out):
         super(MLP, self).__init__()
